a40caffe9d241fdd849f95bc93380749be967b17
Jocelyn <jocelyn.roul@etu.univ-nantes.fr>
2020-08-05 01:06:16 +0200

    feat : appel api

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,38 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
 
+const getWebtoons = async () => {
+    const response = await fetch('http://localhost:8080/webtoons');
+    return response.json();
+};
+
+const Webtoon = ({webtoon}: any) => {
+    const {id, name} = webtoon;
+    return(
+        <div>
+            {id}{name}
+        </div>
+    )
+}
+
+const App = () => {
+
+    const [webtoons, setWebtoons] = useState([]);
+
+    useEffect(() => {
+        getWebtoons().then(res => setWebtoons(res));
+    }, []);
+
+    return(
+        <div>
+            {webtoons.map((webtoon: any) => (
+                <Webtoon key={webtoon.id} webtoon={webtoon}/>
+            ))}
+        </div>
+    )
+};
+
 ReactDOM.render(
-    <h1>Bonjour, monde !</h1>,
+    <App />,
     document.getElementById('root')
 );
